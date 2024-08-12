798a4683bbace98cfb53756055a62fd1ca3d1461
Jeronimo Pueyrredon <jerojp@hotmail.com>
2024-08-11 23:34:19 -0300

    Agregue Models

--- centromed/models.py
+++ centromed/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Medico(models.Model):
+    nombre= models.CharField(max_length=20)
+    apellido= models.CharField(max_length=20)
+    matricula = models.IntegerField()
+    especialidad= models.CharField (max_length=20)
+    email= models.EmailField()
+
+class Paciente (models.Model):
+    nombre= models.CharField(max_length=20)
+    apellido= models.CharField(max_length=20)
+    ident= models.IntegerField()
+    email= models.EmailField()
+    habitacion= models.IntegerField()
+
+class Obrasocial(models.Model):
+    nombre= models.CharField (max_length=20)
+    plan= models.IntegerField()
+    cobertura= models.BooleanField()
+
+class Farmacia (models.Model):
+    medicamento= models.CharField(max_length= 20)
+    droga= models.CharField (max_length= 20)
+    receta= models.BooleanField()
+
