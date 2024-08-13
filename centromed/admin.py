07d97bcc563af128232323e38d04746c8fce59e7
Jeronimo Pueyrredon <jerojp@hotmail.com>
2024-08-13 00:46:27 -0300

    Modifique Admin List Display y list per page

--- centromed/admin.py
+++ centromed/admin.py
@@ -4,6 +4,22 @@ from .models import *
 
 # Register your models here.
 
+class MedicoAdmin(admin.ModelAdmin):
+    list_display = ("nombre")
+    list_per_page = 10
+
+class PacienteAdmin(admin.ModelAdmin):
+    list_display = ("nombre")
+    list_per_page = 10
+
+class ObrasocialAdmin(admin.ModelAdmin):
+    list_display = ("nombre")
+    list_per_page = 10
+
+class FarmaciaAdmin(admin.ModelAdmin):
+    list_display = ("medicamento")
+    list_per_page = 10        
+
 admin.site.register(Medico)
 admin.site.register(Paciente)
 admin.site.register(Farmacia)
--- centromed/models.py
+++ centromed/models.py
@@ -10,6 +10,9 @@ class Medico(models.Model):
     especialidad= models.CharField (max_length=20)
     email= models.EmailField()
 
+    def __str__(self):
+        return f"Nombre del Profesional: {self.nombre} - Matricula: {self.matricula}"
+
 class Paciente (models.Model):
     nombre= models.CharField(max_length=20)
     apellido= models.CharField(max_length=20)
@@ -17,13 +20,22 @@ class Paciente (models.Model):
     email= models.EmailField()
     habitacion= models.IntegerField()
 
+    def __str__(self):
+        return f"Nombre del Paciente: {self.nombre} - Identificacion del Paciente: {self.ident}"
+
 class Obrasocial(models.Model):
     nombre= models.CharField (max_length=20)
     plan= models.IntegerField()
     cobertura= models.BooleanField()
 
+    def __str__(self):
+        return f"Nombre de la Obra Social: {self.nombre} - Cobertura de la Obra Social: {self.cobertura}"
+
 class Farmacia (models.Model):
     medicamento= models.CharField(max_length= 20)
     droga= models.CharField (max_length= 20)
     receta= models.BooleanField()
+
+    def __str__(self):
+        return f"Nombre del Medicamento: {self.medicamento} - Droga: {self.droga}"
 
