730c0e746b274da71e04756c9a8fc85e77686691
Jeronimo Pueyrredon <jerojp@hotmail.com>
2024-08-15 19:10:41 -0300

    Agregue Logout y verifique su funcionamiento

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from users.views import *
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("login/", login_request, name= "Login"),
     path('register', register, name='Register'),
+    path('logout', LogoutView.as_view(template_name='centromed/index.html')),
+
     ]
