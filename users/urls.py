8a7804aef10ebba859d2721c5a699f2aad3db79f
Jeronimo Pueyrredon <jerojp@hotmail.com>
2024-08-16 02:36:35 -0300

    Agregue el cambiar Pass y Verifique su funcionamiento

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("login/", login_request, name= "Login"),
     path('register/', register, name='Register'),
     path('logout', LogoutView.as_view(template_name='centromed/index.html'), name="Logout"),
-    path('editar_perfil/', editar_perfil, name="EditarPerfil")
+    path('editar_perfil/', editar_perfil, name="EditarPerfil"),
+    path('editar_pass', CambiarConstrasenia.as_view(), name="CambiarContraseña")
 
     ]
--- users/views.py
+++ users/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import PasswordChangeView
+from django.urls import reverse_lazy
 
 from users.forms import *
 
@@ -60,3 +63,8 @@ def editar_perfil (request):
         mi_formulario = UserEditForm(instance=usuario)
     
     return render (request, "users/editar_perfil.html", {"form":mi_formulario})
+
+class CambiarConstrasenia(LoginRequiredMixin, PasswordChangeView):
+    template_name= 'users/editar_pass.html'
+    success_url= reverse_lazy("EditarPerfil")
+
